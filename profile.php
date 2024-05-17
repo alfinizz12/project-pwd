70698a41ef305887067721a2e23a18ae5521b1c9
wntrZero <aurelia.rana111@gmail.com>
2024-05-17 22:22:58 +0700

    Update profile.php
    update

--- profile.php
+++ profile.php
@@ -7,6 +7,20 @@ if (!isset($_SESSION['username'])) {
     header("Location: login.php");
 }
 
+if(isset($_POST['save'])){
+    if(update($_POST)> 0){
+        echo "<script>
+            alert('Berhasil booking!, silahkan cek di profil anda!');
+        </script>";
+        header("Location: profile.php");
+    } else {
+        echo "<script>
+            alert('Gagal booking!, silahkan cek kembali tanggal yang diinput!');
+        </script>";
+    }
+
+}
+
 // mengambil user id dengan session
 $uname = $_SESSION['username'];
 $user_id = mysqli_query($connection, "SELECT id FROM user WHERE username = '$uname'");
@@ -15,6 +29,13 @@ $id_self = mysqli_fetch_array($user_id);
 // mengambil user email dengan user id
 $email_user = mysqli_query($connection, "SELECT email FROM user WHERE username = '$uname'");
 $email = mysqli_fetch_array($email_user);
+
+$pass_user = mysqli_query($connection, "SELECT password FROM user WHERE username = '$uname'");
+$password = mysqli_fetch_array($pass_user);
+
+// profile pict dari database 
+$prof_user = mysqli_query($connection, "SELECT photo FROM user WHERE username = '$uname'");
+$photo = mysqli_fetch_array($prof_user);
 
 // perintah memilih row pada tabel resort
 $resort_data = mysqli_query($connection, "SELECT * FROM resort_booking WHERE user_id_resort = '$id_self[0]' ");
@@ -43,7 +64,7 @@ $count_data = mysqli_num_rows($resort_data);
     </div>
     <div class="prof-inside">
         <div class="my-profile">
-            <img src="img/profil.jpeg" alt="">
+            <img src="img/<?= $photo[0] ?>">
             <div class="uname-profile">
                 <p>Username</p>
                 <label for="">
@@ -58,99 +79,123 @@ $count_data = mysqli_num_rows($resort_data);
             </div>
             <button class="editprof" onclick="popup()">edit</button>
             <div class="logout-btn">
-                <?php if (isset($_SESSION['username'])) {
-                    echo "<form action='logout.php'><button>log out</button></form>";
-                }
-                ?>
+                <form action='logout.php'><button onclick = "return confirm('Yakin ingin logout? Anda akan dikembalikan ke landing page')">log out</button></form>
             </div>
         </div>
 
         <div class="booking-list">
             <div class="act-bookinglist">
+                <div class="vertical-menu">
+                    <?php if (isset($def)) echo $def; ?>
+                    <?php while ($resort_data_fetch = mysqli_fetch_assoc($resort_data)) : ?>
+                        <?php
+                        if ($resort_data_fetch['resort_id'] == 1) {
+                            $room_type = "Standard";
+                        } else if ($resort_data_fetch['resort_id'] == 2) {
+                            $room_type = "Deluxe";
+                        } else if ($resort_data_fetch['resort_id'] == 3) {
+                            $room_type = "Suite";
+                        }
+                        ?>
 
+                        <div class="ticket-book">
+                            <h2>My Ticket</h2>
+                            <div class="ticket-up">
+                                <h6>ticket type</h6>
+                            </div>
+                            <div class="ticket-up">
+                                <h3>Room Booking</h3>
+                                <h3><?= $room_type ?> Room</h3>
+                            </div>
+                            <div>
+                                <table class="ticket-inside">
+                                    <tr>
+                                        <td>Check-in date</td>
+                                        <td>Check-out date</td>
+                                        <td>Total</td>
+                                        <td rowspan="2"><i class="bi bi-qr-code"></i></td>
+                                    </tr>
+                                    <tr>
+                                        <?php 
+                                            $resort_data_fetch['date'] = date("d M Y", strtotime($resort_data_fetch['date']));
+                                            
+                                            $resort_data_fetch['date_out'] = date("d M Y", strtotime($resort_data_fetch['date_out']));
+
+                                            $resort_data_fetch['total'] = 'Rp. '. number_format($resort_data_fetch['total'], 0, ',', ',');
+
+                                        ?>
+                                        <td><?= $resort_data_fetch['date']; ?></td>
+                                        <td><?= $resort_data_fetch['date_out']; ?></td>
+                                        <?php ?>
+                                        <td><?= $resort_data_fetch['total']; ?></td>
+                                    </tr>
+                                    <tr></tr>
+                                </table>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
             </div>
-            <div class="rst-bookinglist">
 
+            <?php
+            // Re-fetching the data for the second loop
+            $resort_data = mysqli_query($connection, "SELECT * FROM resort_booking WHERE user_id_resort = '$id_self[0]' ");
+            ?>
+            <div class="rst-bookinglist">
+                
+                <div class="vertical-menu">
+                    <?php if (isset($def)) echo $def; ?>
+                    <?php while ($resort_data_fetch = mysqli_fetch_assoc($resort_data)) : ?>
+                        <?php
+                        if ($resort_data_fetch['resort_id'] == 1) {
+                            $room_type = "Standard";
+                        } else if ($resort_data_fetch['resort_id'] == 2) {
+                            $room_type = "Deluxe";
+                        } else if ($resort_data_fetch['resort_id'] == 3) {
+                            $room_type = "Suite";
+                        }
+                        ?>
+
+                        <div class="ticket-book">
+                            <h2>My Ticket</h2>
+                            <div class="ticket-up">
+                                <h6>ticket type</h6>
+                            </div>
+                            <div class="ticket-up">
+                                <h3>Activity Booking</h3>
+                                <h3><?= $room_type ?> Room</h3>
+                            </div>
+                            <div>
+                                <table class="ticket-inside">
+                                    <tr>
+                                        <td>Check-in date</td>
+                                        <td>Check-out date</td>
+                                        <td>Total</td>
+                                        <td rowspan="2"><i class="bi bi-qr-code"></i></td>
+                                    </tr>
+                                    <tr>
+                                    <?php 
+                                            $resort_data_fetch['date'] = date("d M Y", strtotime($resort_data_fetch['date']));
+                                            
+                                            $resort_data_fetch['date_out'] = date("d M Y", strtotime($resort_data_fetch['date_out']));
+
+                                            $resort_data_fetch['total'] = 'Rp. '. number_format($resort_data_fetch['total'], 0, ',', ',');
+
+                                        ?>
+                                        <td><?= $resort_data_fetch['date']; ?></td>
+                                        <td><?= $resort_data_fetch['date_out']; ?></td>
+                                        <?php ?>
+                                        <td><?= $resort_data_fetch['total']; ?></td>
+                                    </tr>
+                                    <tr></tr>
+                                </table>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
             </div>
         </div>
     </div>
-
-
-
-
-
-    <!-- <div class="profile-box">
-        <div class="profile-pic">
-            <img src="img/profil.jpeg" alt="">
-            <div class="uname-prof">
-                <h3><?= $_SESSION['username'] ?></h3>
-                <p><?= $email[0] ?></p>
-            </div>
-            <div>
-                <button class="editprof" onclick="popup()"><u>edit</u></button>
-            </div>
-            <div class="logout-btn">
-                <?php if (isset($_SESSION['username'])) {
-                    echo "<form action='logout.php'><button>log out</button></form>";
-                }
-                ?>
-
-            </div>
-        </div>
-        <div class="mybooking">
-            <h2>My Bookings</h2>
-            <?php
-            if ($count_data === 0) {
-                // echo "KOSONG";
-                $def = "<div class='the-booking'>
-                <h2>No Booking</h2>
-                </div>";
-            }
-            ?>
-
-            <div class="vertical-menu">
-                <?php if (isset($def)) echo $def; ?>
-                <?php while ($resort_data_fetch = mysqli_fetch_assoc($resort_data)) : ?>
-                    <?php
-                    if ($resort_data_fetch['resort_id'] == 1) {
-                        $room_type = "Standard";
-                    } else if ($resort_data_fetch['resort_id'] == 2) {
-                        $room_type = "Deluxe";
-                    } else if ($resort_data_fetch['resort_id'] == 3) {
-                        $room_type = "Suite";
-                    }
-                    ?>
-
-                    <div class="ticket-book">
-                        <h2>My Ticket</h2>//
-                        <div class="ticket-up">
-                            <h6>ticket type</h6>
-                        </div>
-                        <div class="ticket-up">
-                            <h3>Room Booking</h3>
-                            <h3><?= $room_type ?> Room</h3>
-                        </div>
-                        <div>
-                            <table class="ticket-inside">
-                                <tr>
-                                    <td>Check-in date</td>
-                                    <td>Check-out date</td>
-                                    <td>Total</td>
-                                    <td rowspan="2"><i class="bi bi-qr-code"></i></td>//
-                                </tr>
-                                <tr>
-                                    <td><?= $resort_data_fetch['date']; ?></td>
-                                    <td><?= $resort_data_fetch['date_out']; ?></td>
-                                    <td><?= $resort_data_fetch['total']; ?></td>
-                                </tr>
-                                <tr></tr>
-                            </table>
-                        </div>
-                    </div>
-                <?php endwhile; ?>
-            </div>
-        </div>
-    </div> -->
 
     <!-- modal edit profile  -->
     <div class="popup-edit" id="popup-edit1">
@@ -158,10 +203,15 @@ $count_data = mysqli_num_rows($resort_data);
         <div class="edit-content">
             <div class="close-btn" onclick="popup()">&times;</div>
             <h2>Edit My Profile</h2>
-            <form action="" enctype="multipart/form-data">
-                <img class="image-edit" src="img/profil.jpeg" alt="">
-                <input type="file" name="foto" id="foto">
-                <input type="text" name="username" class="input-field" placeholder="Username" required>
+            <form action="" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="id" value="<?= $id_self[0]?>">
+                <input type="hidden" name="email" value="<?= $email[0]?>">
+                <input type="hidden" name="password" value="<?= $password[0] ?>">
+                <input type="hidden" name="fotoLama" value="<?=$profpic[0]?>">
+                <img class="image-edit" src="img/<?= $photo[0]?>" alt="" id="prof-pic">
+                <label class="update-img" for="input-foto">Update Image</label>
+                <input class="link-img-update" type="file" accept="img/jpeg, img/jpg, img/png" name="foto" id="input-foto">
+                <input type="text" name="username" class="input-field" placeholder="Username" value="<?=$uname?>" required>
                 <button type="submit" class="login-button" name="save">Save</button>
             </form>
         </div>
@@ -171,6 +221,14 @@ $count_data = mysqli_num_rows($resort_data);
         function popup() {
             document.getElementById("popup-edit1").classList.toggle("active");
         }
+
+        let profilePic = document.getElementById("prof-pic");
+        let inputFoto = document.getElementById("input-foto");
+
+        inputFoto.onchange = function(){
+            profilePic.src = URL.createObjectURL(inputFoto.files[0]);
+        }
+
     </script>
 
 </body>
